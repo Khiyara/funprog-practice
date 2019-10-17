ffe7cbea17e1865bc5ec214c90939c5a71eba38a
Khiyara <rizkhiphastiputra@gmail.com>
2019-10-17 21:44:52 +0700

    add evaluate.hs

---
+++ latihan_uts/evaluate.hs
@@ -0,0 +1,14 @@
+data Expr = C Float 
+          | Expr :+ Expr 
+          | Expr :- Expr
+          | Expr :* Expr 
+          | Expr :/ Expr
+
+evaluate::Expr -> Float
+evaluate (C x) = x
+evaluate (e1 :+ e2) = evaluate e1 + evaluate e2
+evaluate (e1 :- e2) = evaluate e1 - evaluate e2
+evaluate (e1 :* e2) = evaluate e1 * evaluate e2
+evaluate (e1 :/ e2) = evaluate e1 / evaluate e2
+
+e1 = (C 10 :+ (C 8 :/ C 2)) :* (C 7 :- C 5)
